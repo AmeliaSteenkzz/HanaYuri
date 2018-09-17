e6ec58a901aa619b30e2a334f6e1d2a8736d62c6
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2018-09-17 09:54:55 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
   //tempmute @user 1s/m/h/d
 
-  let tomute = message.guild.member(message.mentions.users..first() || message.guild.members.get(args[0]));
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(tomute) return message.reply("Couldn't find user.");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
   let muterole = message.guild.roles.find('name', "muted");
