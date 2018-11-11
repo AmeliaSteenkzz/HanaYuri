095cbb15661180ea2ef47c4bd94f9ca04388f535
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2018-11-11 20:43:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 
-  bot.user.setActivity("h!help for more info", {type: "WATCHING"});
+  bot.user.setActivity("Jesus was black", {type: "WATCHING"});
 
   //bot.user.setGame("HW Original Bot");
 });
