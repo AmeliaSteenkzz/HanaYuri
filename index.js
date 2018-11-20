23b0e5696988d2d2b64100d7af82dd7e80fe67c9
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2018-11-20 21:57:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ bot.on("ready", async () => {
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
-  if (!message.content.startsWith(prefix)) return;
 
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
+  if (!message.content.startsWith(prefix)) return;
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 
