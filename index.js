2a5f58fadf699266fabf35b5ad11b98d3aec0443
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2018-11-20 21:37:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ bot.on("ready", async () => {
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
+  if (!message.content.startsWith(prefix)) return;
 
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
