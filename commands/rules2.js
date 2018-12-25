6b0975cc390fa480795964c9e251c893449c4904
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2018-12-25 16:24:41 +0100

    Update rules2.js

--- commands/rules2.js
+++ commands/rules2.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("2.C", "Using racist words is allowed, except if the person your talking too is not cool with it.")
         .addField("2.D", "Do not send unwanted dickpics or nudes to people. If you do send unwanted nudes, then we are forced to remove you of the server.")
         .addField("2.E", "If we find out you take part in Pedophilia like actions in real life, we are forced to remove you of the server.")
-        .addField("2.F", "Do not pretend to be someone else. Aka identity theft. This could lead to a ban.")
+        .addField("2.F", "Do not pretend to be someone else. Aka identity theft or catfishing. This could lead to a ban.")
         .addField("2.G", "Disrespecting Staff members could get you perma muted.");
 
         return message.channel.send(serverembed);
