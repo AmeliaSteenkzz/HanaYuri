9fd8f64d0cff99e6419f93270ac2bd776c99cc22
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-04 18:17:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+let coins = require("./coins.json");
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -25,12 +26,30 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 
-  bot.user.setActivity("Hentai World", {type: "PLAYING"});
+  bot.user.setActivity("Hentai World", {type: "WATCHING"});
 
-  //bot.user.setGame("HW Original Bot");
+});
+
+bot.on("guildMemberAdd", async member => {
+  console.log(`${member.id} joined the server.`);
+
+  let welcomechannel = member.guild.channels.find(`name`, "staff_bot_commands");
+  welcomechannel.send(`${member} has joined the server.`);
+});
+
+bot.on("channelCreate", async channel => {
+
+  console.log(`${channel.name} has been created.`);
+
+  let logsEmbed = new Discord.RichEmbed()
+  let sChannel = channel.guild.channels.find(`name`, "test");
+  sChannel.send(logsEmbed);
+  sChannel.send(`${channel} has been created`);
+
 });
 
 bot.on("message", async message => {
+
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
@@ -43,6 +62,35 @@ bot.on("message", async message => {
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 
-});
+  });
 
-bot.login(process.env.token);
+bot.on("message", async message => {
+
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+}};
+
+let coinAmt = Math.floor(Math.random() * 1) + 1;
+let baseAmt = Math.floor(Math.random() * 1) + 1;
+console.log(`${coinAmt} ; ${baseAmt}`);
+
+if(coinAmt === baseAmt){
+  coins[message.author.id] = {
+    coins: coins[message.author.id].coins + coinAmt
+  };
+fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+  if (err) console.log(err)
+});
+let coinEmbed = new Discord.RichEmbed()
+.setAuthor(message.author.username)
+.setColor("#ff70ff")
+.addField("💰", `${coinAmt} coins added!`);
+
+if(message.author.bot) return;
+let coinschannel = message.guild.channels.find(`name`, "coins");
+if(!coinschannel) return message.channel.send("Couldn't find coins channel.");
+coinschannel.send(coinEmbed);
+}});
+
+bot.login(botconfig.token);
