ddaee9ec6ac19bd4277f94d0a68e144af191dab3
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-05 00:08:19 +0100

    Update rules1.js

--- commands/rules1.js
+++ commands/rules1.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
         .setColor("#ff70ff")
         .setThumbnail(sicon)
         .addField("1.A", "Do not keep all drama and fights in the server.")
-        .addField("1.B", "Do that in Private Messages, unless it has something to do with the server.")
-        .addField("1.C", "If you continue with the drama or the fight, it will rise to a temporary mute. These rules also fall under aggression.");
+        .addField("1.B", "Do that in Private Messages. if you continue with the drama or the fight, then you'll be muted.")
+        .addField("1.C", "We take no responsibility for the events that occur in Pm / Dm, unless it breaks the Discord Tos.");
 
         return message.channel.send(serverembed);
 
