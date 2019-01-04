9c5762c7f4d21aff95184b6000a9525d950002b9
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-05 00:10:43 +0100

    Update rules4.js

--- commands/rules4.js
+++ commands/rules4.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("4.A", "All kinds of Hentai will only be posted in channels with the nsfw tag")
         .addField("4.B", "No Gore, Guro, Vore, Pedophilia, Necrophiliac or other weird disgusting shit.")
         .addField("4.C", "Dont judge other fetishes or hentai. This could get really iritating quick, just respect it, or dont say anything. Otherwise this can lead up to a perma-mute.")
-        .addField("4.D", "You will be given the SFW role when you're 13 years of age or below.");
+        .addField("4.D", "You will be given the SFW role when you're 14 years of age or below.");
 
         return message.channel.send(serverembed);
 
