00044e87a40d1d3330eae9efd8caef75116c1a3c
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-05 17:45:21 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry my dude, you can't do that");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Sorry my dude, you can't do that");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!args[0]) return message.reply("Usage: h!addrole *@user* *role*");
-  if(!rMember) return message.reply("Couldn't find that user m8");
+  if(!args[0]) return message.channel.send("Usage: h!addrole *@user* *role*");
+  if(!rMember) return message.channel.send("Couldn't find that user m8");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role, idiot");
+  if(!role) return message.channel.send("Specify a role, idiot");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
+  if(!gRole) return message.channel.send("Couldn't find that role.");
 
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role");
+  if(rMember.roles.has(gRole.id)) return message.channel.send("They already have that role");
   await(rMember.addRole(gRole.id));
 
 try{
