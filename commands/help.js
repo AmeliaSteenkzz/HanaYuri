6263a752b699bece3c9be4cfa750b83df9affffe
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-05 16:41:33 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -15,8 +15,9 @@ module.exports.run = async (bot, message, args) => {
   let question = args.slice(1).join(" ");
 
   let infoembed = new Discord.RichEmbed()
-  .setTitle("Information Command List")
+  .setDescription("Information Command List")
   .setColor("#af0e97")
+  .setThumbnail("https://cdn.discordapp.com/attachments/486583639253975042/531130449649467393/2000px-Icon-notepad.svg.png")
   .addField("h!help", "shows a list of all my commands")
   .addField("h!bots", "shows a list of all the bots and prefixes in the Hentai World")
   .addField("h!rules", "shows all the rules in the server")
@@ -30,20 +31,23 @@ module.exports.run = async (bot, message, args) => {
   .addField("h!ping", "shows the response time of Hanayuri");
 
   let funembed = new Discord.RichEmbed()
-  .setTitle("Fun Command List")
+  .setDescription("Fun Command List")
   .setColor("#af0e97")
+  .setThumbnail("https://cdn.discordapp.com/attachments/486583639253975042/531132326638387201/b8316909587b624d18cd4c52b27c653e.png")
   .addField("h!8ball", "ask Hanayuri a question")
   .addField("h!cat", "shows a random cat picture")
-  .addField("h!doggo", "shows a random doggo picture");
+  .addField("dogembed", "shows a random doggo picture");
 
   let othersembed = new Discord.RichEmbed()
-  .setTitle("Others Command List")
+  .setDescription("Others Command List")
   .setColor("#af0e97")
+  .setThumbnail("https://cdn.discordapp.com/attachments/486583639253975042/531133221992398868/854145.png")
   .addField("h!report", "report someone in the server");
 
   let staffhelpembed = new Discord.RichEmbed()
   .setDescription("Staff Command List")
   .setColor("#af0e97")
+  .setThumbnail("https://cdn.discordapp.com/attachments/486583639253975042/531134577675337748/wrench-512.png")
   .addField("h!say", "let Hanayuri say something")
   .addField("h!clear", "delete an ammount of messages in the chat")
   .addField("h!addrole", "give someone a role")
