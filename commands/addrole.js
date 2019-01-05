b46d0918df865ac3c09f5b445d76fa9f61246b6f
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-05 17:25:52 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  //!addole @fleur No Game
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry my dude, you can't do that");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!args[0]) return message.reply("Usage: h!addrole *@user* *role*");
   if(!rMember) return message.reply("Couldn't find that user m8");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
+  if(!role) return message.reply("Specify a role, idiot");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Couldn't find that role.");
 
@@ -15,7 +15,7 @@ module.exports.run = async (bot,message,args) => {
   await(rMember.addRole(gRole.id));
 
 try{
-  await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+  await rMember.send(`Congrats, you have been given ${gRole.name}. Thank you for donating to Hentai World!`)
 }catch(e){
   message.channel.send(`Added ${gRole.name} to <@${rMember.id}>`)
   }
