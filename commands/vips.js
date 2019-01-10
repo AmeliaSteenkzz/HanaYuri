c734135ac9323dc2edf44e6c99ebc1f91cfe6b43
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-10 19:58:20 +0100

    Update vips.js

--- commands/vips.js
+++ commands/vips.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let vipEmbed = new Discord.RichEmbed()
+   .setTitle("Click here to donate")
+   .setURL("https://donatebot.io/checkout/403133673408299009?buyer=246693689731186688")
    .setColor("#af0e97")
    .setImage("https://cdn.discordapp.com/attachments/486583639253975042/530507515713028114/unknown.png");
 
