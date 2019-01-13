8fb18829d70be533ba65183afcfdcbc4417eb974
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-13 23:27:21 +0100

    Add files via upload

---
+++ commands/rank.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+let xp = require("../xp.json");
+
+module.exports.run = async (bot, message, args) => {
+
+ if(!xp[message.author.id]){
+ xp[message.author.id] = {
+   xp: 0,
+   level: 1
+ };
+}
+ let curxp = xp[message.author.id].xp;
+ let curlvl = xp[message.author.id].level;
+ let nxtLvlXp = curlvl * 150;
+ let difference = nxtLvlXp - curxp;
+
+ let lvlEmbed = new Discord.RichEmbed()
+ .setAuthor(message.author.username)
+ .setColor("#af0e97")
+ .setThumbnail(message.author.displayAvatarURL)
+ .addField("Level", curlvl, true)
+ .addField("XP", curxp, true)
+ .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
+
+ return message.channel.send(lvlEmbed);
+}
+module.exports.help = {
+  name: "rank"
+}
