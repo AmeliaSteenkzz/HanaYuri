17507d84c4e04d4c143763853c262ae503d19989
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-06-18 13:45:50 +0200

    Update bots.js

--- commands/bots.js
+++ commands/bots.js
@@ -3,12 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let helpembed = new Discord.RichEmbed()
-  .setDescription("Use the prefix from a bot with the help command to get more info.")
   .setColor("#af0e97")
-  .addField("Normal Bots", "`42 [+]` `AmariBot [!]` `Ayana [=]` `Ayana Beta [=]` `Carl-bot [!rr]` `Dank Memer [pls ]` `Donate Bot [donate]` `Dyno [?]` `Hanayuri [h!]` `Jeanne [s/]` `Koya [^^]` `Miki [>]` `Nadeko [.]` `Namiko [!]` `Nekobot [n!]` `Paisley Park [-]` `Park Battles [-]` `Pokécord [p!]` `Pollux [+]` `qtChan [-]` `Tatsumaki [t!]` `Yggdrasil [--]` `ZeroTwo [zt!]`")
-  .addField("Music Bots", "`FredBoat♪♪ [;;]` `Mantaro [->]` `Rythm []`")
-  .addField("Waifu Bots", "`Mudae [$]` `Mudamaid4 [$]`")
-  .addField("Bump Bots", "` []`")
+  .addField("Bots", `:${message.guild.roles.get('536579349474836483').members.map(m=>m.user).join('\n')}`)
   return message.channel.send(helpembed);
 }
 module.exports.help = {
