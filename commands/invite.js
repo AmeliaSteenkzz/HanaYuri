fe968d2a71e5cd0f3661c842aca161902847e1a2
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-06-18 13:46:48 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,11 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let invEmbed = new Discord.RichEmbed()
-  .setDescription("Server Invites")
+  .setDescription("Server Invite")
   .setColor("#af0e97")
   .setThumbnail(message.guild.iconURL)
-  .addField("Instant Invite", `https://discord.gg/kaVUygM`)
-  .addField("Discord.io", `https://discord.io/animeworld`)
+  .addField("Instant Invite", `https://discord.gg/zVaAfMd`)
   .setFooter(`requested by ${message.author.tag}`, message.author.displayAvatarURL);
   message.channel.send(invEmbed);
 
