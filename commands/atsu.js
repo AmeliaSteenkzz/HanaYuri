b8864c12d82289830a87f613824ff7150e079d38
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-09-22 15:34:08 +0200

    Update atsu.js

--- commands/atsu.js
+++ commands/atsu.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-number = 29;
+number = 33;
 imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
 
 message.channel.send ( {files: ["./atsu/" + imageNumber + ".jpg"]} )
