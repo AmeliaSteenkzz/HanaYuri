5281b6119a25e935a6a394f0b83f5b22e2a05885
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2020-03-28 22:12:22 +0100

    Update danae.js

--- commands/danae.js
+++ commands/danae.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-number = 12;
+number = 15;
 imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
 
 message.channel.send ( {files: ["./danae/" + imageNumber + ".jpg"]} )
