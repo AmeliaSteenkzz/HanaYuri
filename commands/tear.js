c501d43634e138a6bc87212e42e084cea9439e9d
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2020-04-07 21:45:25 +0200

    Create tear.js

---
+++ commands/tear.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+number = 7;
+imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
+
+message.channel.send ( {files: ["./tear/" + imageNumber + ".jpg"]} )
+
+
+}
+
+module.exports.help = {
+  name: "tear"
+}
