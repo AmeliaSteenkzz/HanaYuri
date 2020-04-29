2f73536d1347d9d0aab3b06f9a84f772153329ba
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2020-04-29 15:30:20 +0200

    Update jacob.js

--- commands/jacob.js
+++ commands/jacob.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-number = 7;
+number = 8;
 imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
 
 message.channel.send ( {files: ["./Jacob/" + imageNumber + ".jpg"]} )
