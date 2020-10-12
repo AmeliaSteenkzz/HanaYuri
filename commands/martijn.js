c3bea676d6ec54937ae84e0ea47aa8f4d493760d
Steenkzz <32714415+MartijnSteenkz@users.noreply.github.com>
2020-10-12 12:16:58 +0200

    Update martijn.js

--- commands/martijn.js
+++ commands/martijn.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-number = 32;
+number = 34;
 imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
 
 message.channel.send ( {files: ["./martijn/" + imageNumber + ".jpg"]} )
