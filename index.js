5f82c642be0e99b2eba7e6aaab10e8e5285d2aef
Martinus <32714415+steenkzz@users.noreply.github.com>
2021-08-18 15:57:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const botconfig = require("./botconfig.json");
-const Discord = require("discord.js")({
-    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
-});
+const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({ disableEveryone: true });
 const token = process.env.token;
+const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
 bot.commands = new Discord.Collection();
 
 
